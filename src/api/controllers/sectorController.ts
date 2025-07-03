c4c9401d50a21d29fdc9cf8e9ef16c7372c0c9ec
AndersonL95 <anderson-alves@hotmail.com>
2025-07-02 22:21:03 -0300

    create deleteSector

--- src/api/controllers/sectorController.ts
+++ src/api/controllers/sectorController.ts
@@ -39,6 +39,17 @@ export const listSector = async (req: Request, res: Response) => {
     res.status(200).send(sector);
 }
 
+export const deleteSector = async (req: Request, res: Response) => {
+    const sectorId = parseInt(req.params.id);
+    const tenantId = req.body.tenantId;
+    console.log("DATA: ", sectorId, "TENANTID: ", tenantId);
+
+    const sector = await Sector.findOne({where: {id: sectorId, tenantId}});
+    if(!sector) return res.status(404).send("Setor não encontrado.");
+
+    await sector.remove();
+    res.status(200).send("Setor excluido.");
+};
 
 
 
